dad24566628f03b7979d89564477bce058828417
Elliot Peele <http://bugs.rpath.com/>
2007-04-18 23:40:15 -0400

    add rap ras plugin

---
+++ amiconfig/plugins/raprsakey.py
@@ -0,0 +1,24 @@
+#
+# Copyright (c) 2007 rPath, Inc.
+#
+
+from rpathplugin import rPathPlugin
+
+class RAPRSAKey(rPathPlugin):
+    name = 'raprsakey'
+
+    def pluginMethod(self):
+        if ('rappublickey' not in self.rpathcfg or
+            'rapprivatekey' not in self.rpathcfg):
+            return
+
+        pub = self.rpathcfg['rappublickey']
+        priv = self.rpathcfg['rapprivatekey']
+
+        pubfile = open('/etc/raa/raa-service.pubkey', 'w')
+        pubfile.write(pub)
+        pubfile.close()
+
+        privfile = open('/etc/raa/raa-service.privkey', 'w')
+        privfile.write(priv)
+        privfile.close()
