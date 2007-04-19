4d5ed1b8dc52daaeecde09788765525b0d214d54
Elliot Peele <http://bugs.rpath.com/>
2007-04-18 22:45:15 -0400

    add conary proxy plugin

---
+++ amiconfig/plugins/conaryproxy.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2007 rPath, Inc.
+#
+
+from amiconfig.errors import *
+from rpathplugin import rPathPlugin
+
+class AMIConfigPlugin(rPathPlugin):
+    name = 'conaryproxy'
+
+    def pluginMethod(self):
+        if 'conaryproxy' not in self.rpathcfg:
+            return
+
+        proxy = self.rpathcfg['conaryproxy']
+
+        fh = open('/etc/conary/config.d/amiconfig', 'a')
+        fh.write('proxy %s' % proxy)
+        fh.close()
