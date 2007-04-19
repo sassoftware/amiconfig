4dd72145c9d4f1d941193c01d486ffed0ecb21f9
Elliot Peele <http://bugs.rpath.com/>
2007-04-18 23:09:08 -0400

    fix tabs

--- amiconfig/base64.py
+++ amiconfig/base64.py
@@ -5,7 +5,7 @@
 import base64
 
 def encode(self, s):
-        return base64.encodestring(s).replace('\n', ' ')
+    return base64.encodestring(s).replace('\n', ' ')
 
 def decode(self, s):
     return base64.decodestring(s.replace(' ', '\n'))
