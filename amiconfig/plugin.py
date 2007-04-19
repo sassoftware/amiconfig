111dd3e3205b423eca0332952b75d8da48bb2c6f
Elliot Peele <http://bugs.rpath.com/>
2007-04-18 23:17:05 -0400

    add decoder method for options that we know should be encoded

--- amiconfig/plugin.py
+++ amiconfig/plugin.py
@@ -2,12 +2,17 @@
 # Copyright (c) 2007 rPath, Inc.
 #
 
+import encoding
+
 class AMIPlugin(object):
     name = None
 
     def __init__(self, id, ud):
         self.id = id
         self.ud = ud
+
+    def decode(self, s):
+        return encoding.decode(s)
 
     # Method for plugins to implement.
     def configure(self):
