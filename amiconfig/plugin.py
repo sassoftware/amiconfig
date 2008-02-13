37d2a3984b09027a540eba7f5e24a944d620601c
Elliot Peele <http://issues.rpath.com/>
2008-02-12 22:26:44 -0500

    encoding moved into util

--- amiconfig/plugin.py
+++ amiconfig/plugin.py
@@ -2,17 +2,12 @@
 # Copyright (c) 2007 rPath, Inc.
 #
 
-import encoding
-
 class AMIPlugin(object):
     name = None
 
     def __init__(self, id, ud):
         self.id = id
         self.ud = ud
-
-    def decode(self, s):
-        return encoding.decode(s)
 
     # Method for plugins to implement.
     def configure(self):
