cb3a8a07305b15bbaf275269c926dc04ba5f472d
Elliot Peele <http://issues.rpath.com/>
2008-03-13 16:37:51 -0400

    add hostname plugin for setting the hostname of the machine, by default if nothing is specified it will set the hostname to the fqdn of the private interface.

---
+++ amiconfig/plugins/hostname.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2008 rPath Inc.
+#
+
+from amiconfig.errors import *
+from amiconfig.lib import util
+from amiconfig.plugin import AMIPlugin
+
+class AMIConfigPlugin(AMIPlugin):
+    name = 'hostname'
+
+    def configure(self):
+        try:
+            cfg = self.ud.getSection('hostname')
+            hostname = cfg['hostname']
+        except (EC2DataRetrievalError, KeyError):
+            try:
+                hostname = self.id.getLocalHostname()
+            except EC2DataRetrievalError:
+                return
+
+        util.call('hostname %s' % hostname)
